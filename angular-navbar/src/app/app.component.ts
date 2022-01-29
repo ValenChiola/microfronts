0176bc25f9632b099ac0a2ef5078585d176e1ee3
Agustin N Fiordelisi <fiordex@MacBook-Pro-de-Agustin.local>
2022-01-29 19:05:48 -0300

    Adding angular features

--- angular-navbar/src/app/app.component.ts
+++ angular-navbar/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-navbar';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+}
 }
